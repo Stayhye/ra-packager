a269d93e2227b93c681646a6e17a2c7250956874
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-04-22 21:14:46 -0500

    Update mame2003-plus.sh

--- cores/mame2003-plus.sh
+++ cores/mame2003-plus.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # mame2003-plus.sh
 
+# --- FIX: Define PS2 toolchain paths ---
+export PS2DEV=/usr/local/ps2dev
+export PS2SDK=$PS2DEV/ps2sdk
+export PATH=$PATH:$PS2DEV/bin:$PS2DEV/ee/bin:$PS2DEV/iop/bin:$PS2DEV/dvp/bin:$PS2SDK/bin
+
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 REPO_URL="https://github.com/libretro/mame2003-plus-libretro"
@@ -14,8 +19,8 @@ else
 fi
 
 ## Compile core for PS2
-# MAME 2003-Plus is large; ensuring a clean build is vital.
-make -f Makefile -j $PROC_NR platform=ps2 clean || { exit 1; }
-make -f Makefile -j $PROC_NR platform=ps2 || { exit 1; }
+# We explicitly pass CC and CXX to ensure the Makefile uses the correct PS2 compilers
+make -f Makefile -j $PROC_NR platform=ps2 CC=ee-gcc CXX=ee-g++ AR=ee-ar clean || { exit 1; }
+make -f Makefile -j $PROC_NR platform=ps2 CC=ee-gcc CXX=ee-g++ AR=ee-ar || { exit 1; }
 
 cd .. || { exit 1; }
