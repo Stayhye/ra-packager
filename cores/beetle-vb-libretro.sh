6bdae8a62a6346819552bacef70e56bd3c30a623
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-17 21:03:54 -0500

    Update beetle-vb-libretro.sh

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -4,9 +4,9 @@
 ## Determine the maximum number of processes that Make can work with.
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-## Download or update the source code cleanly.
+## Download or update the source code cleanly using the correct directory name.
 REPO_URL="https://github.com/libretro/beetle-vb-libretro"
-REPO_FOLDER="vb"
+REPO_FOLDER="beetle-vb-libretro"
 BRANCH_NAME="master"
 
 if test ! -d "$REPO_FOLDER"; then
@@ -18,18 +18,14 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
-## Compile core using libretro's generic platform target
+## Compile core
 make -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -j $PROC_NR platform=ps2 || { exit 1; }
 
-## Explicitly package the compiled object files into the static library archive 
-## expected by the PS2 linker using the cross-toolchain archiver
+## Package the compiled object files into the static library archive 
+## directly where generate_retroarch.sh expects it ($1/$2.a -> beetle-vb-libretro/beetle-vb-libretro_ps2.a)
 rm -f beetle-vb-libretro_ps2.a
 mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o mednafen/*.o mednafen/*/*.o libretro-common/*/*.o 2>/dev/null || \
 mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o || { exit 1; }
 
 cd .. || { exit 1; }
-
-## Place it in the directory expected by generate_retroarch.sh ($1/$2.a -> beetle-vb-libretro/beetle-vb-libretro_ps2.a)
-mkdir -p beetle-vb-libretro
-cp "$REPO_FOLDER/beetle-vb-libretro_ps2.a" beetle-vb-libretro/beetle-vb-libretro_ps2.a || { exit 1; }
