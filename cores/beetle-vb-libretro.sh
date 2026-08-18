29fb05352ef718e16a2e0fe86a77c051ae78bc21
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-17 21:43:52 -0500

    Update beetle-vb-libretro.sh

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -23,9 +23,15 @@ make -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -j $PROC_NR platform=ps2 || { exit 1; }
 
 ## Package the compiled object files into the static library archive 
-## directly where generate_retroarch.sh expects it ($1/$2.a -> beetle-vb-libretro/beetle-vb-libretro_ps2.a)
 rm -f beetle-vb-libretro_ps2.a
 mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o mednafen/*.o mednafen/*/*.o libretro-common/*/*.o 2>/dev/null || \
 mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o || { exit 1; }
 
+mips64r5900el-ps2-elf-ranlib beetle-vb-libretro_ps2.a || { exit 1; }
+
 cd .. || { exit 1; }
+
+## Move the final archive into place for generate_retroarch.sh ($1/$2.a)
+mkdir -p beetle-vb-libretro
+rm -f beetle-vb-libretro/beetle-vb-libretro_ps2.a
+cp -f "$REPO_FOLDER/beetle-vb-libretro_ps2.a" beetle-vb-libretro/beetle-vb-libretro_ps2.a || { exit 1; }
