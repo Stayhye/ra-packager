979337217df2679d78cf086bd5da34ac050e2d14
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-17 21:41:35 -0500

    Update beetle-vb-libretro.sh

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # package.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
+## Determine the maximum number of processes that Make can work with.
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
+## Download or update the source code cleanly using the correct directory name.
 REPO_URL="https://github.com/libretro/beetle-vb-libretro"
 REPO_FOLDER="beetle-vb-libretro"
 BRANCH_NAME="master"
@@ -16,34 +18,14 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
-## Clean environment
-make clean || true
+## Compile core
+make -j $PROC_NR platform=ps2 clean || { exit 1; }
+make -j $PROC_NR platform=ps2 || { exit 1; }
 
-## Compile source objects using correct $PS2SDK variables and libretro header locations
-make -j $PROC_NR \
-    CC="mips64r5900el-ps2-elf-gcc" \
-    CXX="mips64r5900el-ps2-elf-g++" \
-    AR="mips64r5900el-ps2-elf-ar" \
-    LD="mips64r5900el-ps2-elf-ld" \
-    CFLAGS="-I$PS2SDK/ee/include -I$PS2SDK/common/include -I." \
-    CXXFLAGS="-I$PS2SDK/ee/include -I$PS2SDK/common/include -I." \
-    SHARED="" \
-    TARGET="beetle-vb-libretro_ps2.a" || { exit 1; }
-
-## Package all generated object files into the static library archive
+## Package the compiled object files into the static library archive 
+## directly where generate_retroarch.sh expects it ($1/$2.a -> beetle-vb-libretro/beetle-vb-libretro_ps2.a)
 rm -f beetle-vb-libretro_ps2.a
-find . -name "*.o" | xargs mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a
-
-if [ ! -f "beetle-vb-libretro_ps2.a" ]; then
-    echo "Error: Failed to generate beetle-vb-libretro_ps2.a archive"
-    exit 1
-fi
-
-mips64r5900el-ps2-elf-ranlib beetle-vb-libretro_ps2.a || { exit 1; }
+mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o mednafen/*.o mednafen/*/*.o libretro-common/*/*.o 2>/dev/null || \
+mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o || { exit 1; }
 
 cd .. || { exit 1; }
-
-## Move the final archive into place for generate_retroarch.sh
-mkdir -p beetle-vb-libretro
-rm -f beetle-vb-libretro/beetle-vb-libretro_ps2.a
-mv -f "$REPO_FOLDER/beetle-vb-libretro_ps2.a" beetle-vb-libretro/beetle-vb-libretro_ps2.a || { exit 1; }
