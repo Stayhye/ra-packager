1fe47ae9e01535c839abadc2d027899dae1f9762
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-17 21:45:25 -0500

    Update beetle-vb-libretro.sh

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -23,9 +23,43 @@ make -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -j $PROC_NR platform=ps2 || { exit 1; }
 
 ## Package the compiled object files into the static library archive 
+## directly where generate_retroarch.sh expects it ($1/$2.a -> beetle-vb-libretro/beetle-vb-libretro_ps2.a)
 rm -f beetle-vb-libretro_ps2.a
 mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o mednafen/*.o mednafen/*/*.o libretro-common/*/*.o 2>/dev/null || \
 mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a *.o || { exit 1; }
+
+cd .. || { exit 1; }#!/bin/bash
+# package.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
+
+## Determine the maximum number of processes that Make can work with.
+PROC_NR=$(getconf _NPROCESSORS_ONLN)
+
+## Download or update the source code cleanly using the correct directory name.
+REPO_URL="https://github.com/libretro/beetle-vb-libretro"
+REPO_FOLDER="beetle-vb-libretro"
+BRANCH_NAME="master"
+
+if test ! -d "$REPO_FOLDER"; then
+    git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER || { exit 1; }
+fi
+
+cd $REPO_FOLDER || { exit 1; }
+git fetch origin
+git reset --hard origin/${BRANCH_NAME}
+git checkout ${BRANCH_NAME} || { exit 1; }
+
+## Compile core normally for platform=ps2
+make -j $PROC_NR platform=ps2 clean || { exit 1; }
+make -j $PROC_NR platform=ps2 || { exit 1; }
+
+## Recursively find and archive ALL compiled object files safely
+rm -f beetle-vb-libretro_ps2.a
+find . -name "*.o" -type f | xargs mips64r5900el-ps2-elf-ar rcs beetle-vb-libretro_ps2.a || { exit 1; }
+
+if [ ! -f "beetle-vb-libretro_ps2.a" ]; then
+    echo "Error: beetle-vb-libretro_ps2.a was not created."
+    exit 1
+fi
 
 mips64r5900el-ps2-elf-ranlib beetle-vb-libretro_ps2.a || { exit 1; }
 
