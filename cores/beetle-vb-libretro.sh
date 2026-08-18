b1d189223a307a476255c046cdcfad6650b857ea
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-17 20:06:14 -0500

    Update beetle-vb-libretro.sh

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -4,26 +4,43 @@
 ## Determine the maximum number of processes that Make can work with.
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-## Download the source code.
+## Download the source code cleanly.
 REPO_URL="https://github.com/libretro/beetle-vb-libretro"
-REPO_FOLDER="beetle-vb-libretro"
+REPO_FOLDER="vb"
 BRANCH_NAME="master"
 
 if test ! -d "$REPO_FOLDER"; then
-	git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER && cd $REPO_FOLDER || { exit 1; }
-else
-	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} && git checkout ${BRANCH_NAME} || { exit 1; }
+	git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER || { exit 1; }
 fi
+
+cd $REPO_FOLDER || { exit 1; }
+git fetch origin
+git reset --hard origin/${BRANCH_NAME}
+git checkout ${BRANCH_NAME} || { exit 1; }
 
 ## Compile core
 make -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -j $PROC_NR platform=ps2 || { exit 1; }
 
-# Rename the compiled output to match what generate_retroarch.sh expects
-if [ -f "mednafen_vb_libretro.a" ]; then
-    cp mednafen_vb_libretro.a beetle-vb-libretro_ps2.a || { exit 1; }
-elif [ -f "beetle_vb_libretro.a" ]; then
-    cp beetle_vb_libretro.a beetle-vb-libretro_ps2.a || { exit 1; }
+## Find whatever archive/library the Makefile built and map it to what generate_retroarch.sh expects
+FOUND_FILE=""
+for f in *.a; do
+    if [ -f "$f" ]; then
+        FOUND_FILE="$f"
+        break
+    fi
+done
+
+if [ -z "$FOUND_FILE" ]; then
+    echo "Error: Could not find any compiled .a static library file!"
+    exit 1
 fi
 
+echo "Found compiled library: $FOUND_FILE. Copying to beetle-vb-libretro_ps2.a"
+cp "$FOUND_FILE" "../beetle-vb-libretro_ps2.a" || { exit 1; }
+
 cd .. || { exit 1; }
+
+# Ensure it's placed where generate_retroarch.sh expects it ($1/$2.a)
+mkdir -p beetle-vb-libretro
+mv beetle-vb-libretro_ps2.a beetle-vb-libretro/beetle-vb-libretro_ps2.a || { exit 1; }
