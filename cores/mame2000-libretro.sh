36fe95bd5e11a694865c4669f37e7c7f70f6b840
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-20 17:16:14 -0500

    Update mame2000-libretro.sh

--- cores/mame2000-libretro.sh
+++ cores/mame2000-libretro.sh
@@ -8,9 +8,9 @@ REPO_FOLDER="mame2000-libretro"
 BRANCH_NAME="ps2-gcc9"
 
 if test ! -d "$REPO_FOLDER"; then
-	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || exit 1
+	git clone --recursive --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || { exit 1; }
 else
-	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} && git checkout ${BRANCH_NAME} || exit 1
+	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} && git checkout ${BRANCH_NAME} && git submodule update --init --recursive || { exit 1; }
 fi
 
 make -j $PROC_NR platform=ps2 clean || exit 1
