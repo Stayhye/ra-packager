61726099215cec5df3e638bd9165d41fa405e3d2
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-20 10:55:58 -0500

    Update beetle-vb-libretro.sh

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -3,7 +3,7 @@
 
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-REPO_URL="https://github.com/libretro/beetle-vb-libretro"
+REPO_URL="https://github.com/Stayhye/beetle-vb-libretro"
 REPO_FOLDER="beetle-vb-libretro"
 BRANCH_NAME="master"
 
