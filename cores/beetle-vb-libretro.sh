3fff5bc53e1bc2fe35d2a36fda55c1b00d7167c4
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-20 11:03:33 -0500

    Update beetle-vb-libretro.sh

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -6,18 +6,28 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 REPO_URL="https://github.com/Stayhye/beetle-vb-libretro"
 REPO_FOLDER="beetle-vb-libretro"
 BRANCH_NAME="master"
+
 if test ! -d "$REPO_FOLDER"; then
-	git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || { exit 1; }
-else
-	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} && git checkout ${BRANCH_NAME} || { exit 1; }
+    git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER || { exit 1; }
 fi
 
-## Compile core
-cd src/libretro || { exit 1; }
+cd $REPO_FOLDER || { exit 1; }
+git fetch origin
+git reset --hard origin/${BRANCH_NAME}
+git checkout ${BRANCH_NAME} || { exit 1; }
+
 make -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -j $PROC_NR platform=ps2 || { exit 1; }
 
-# Trick needed for having subfolders
-cp beetle_vb_libretro_ps2.a ../../beetle_vb_libretro_ps2.a
+## Package all nested object files recursively into the archive
+rm -f libretro_ps2.a
+find . -name "*.o" -type f | xargs mips64r5900el-ps2-elf-ar rcs libretro_ps2.a || { exit 1; }
+mips64r5900el-ps2-elf-ranlib libretro_ps2.a || { exit 1; }
 
-cd ../../../ || { exit 1; }
+cd .. || { exit 1; }
+
+## Copy the archive to both locations (Root for Makefile.ps2 and folder for generate_retroarch.sh)
+cp -f "$REPO_FOLDER/libretro_ps2.a" ./libretro_ps2.a || { exit 1; }
+
+mkdir -p beetle-vb-libretro
+cp -f "$REPO_FOLDER/libretro_ps2.a" beetle_vb_libretro/beetle_vb_libretro_ps2.a || { exit 1; }
