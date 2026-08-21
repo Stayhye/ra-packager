defa32aa1afe8902eb208d206221fc3efd35f9b6
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-20 23:03:08 -0500

    Fix repository URL and update PS2 build process
    Updated repository URL and adjusted file references for the PS2 platform. Modified compilation and packaging steps to use the correct output file.

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -3,7 +3,7 @@
 
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-REPO_URL="https://github.com/Stayhye/beetle-vb-libretro"
+REPO_URL="https://github.com/libretro/beetle-vb-libretro"
 REPO_FOLDER="beetle-vb-libretro"
 BRANCH_NAME="master"
 
@@ -16,19 +16,14 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
-## Compile core (removed -f Makefile.libretro since the file is just Makefile)
+## Compile core using native platform=ps2 support from Makefile
 make -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -j $PROC_NR platform=ps2 || { exit 1; }
 
-## Package all nested object files recursively into the archive
-rm -f libretro_ps2.a
-find . -name "*.o" -type f | xargs mips64r5900el-ps2-elf-ar rcs libretro_ps2.a || { exit 1; }
-mips64r5900el-ps2-elf-ranlib libretro_ps2.a || { exit 1; }
-
 cd .. || { exit 1; }
 
-## Copy the archive to both locations (Root for Makefile.ps2 and folder for generate_retroarch.sh)
-cp -f "$REPO_FOLDER/libretro_ps2.a" ./libretro_ps2.a || { exit 1; }
+## Copy the generated static archive directly (Makefile outputs mednafen_vb_libretro_ps2.a)
+cp -f "$REPO_FOLDER/mednafen_vb_libretro_ps2.a" ./libretro_ps2.a || { exit 1; }
 
 mkdir -p beetle-vb-libretro
-cp -f "$REPO_FOLDER/libretro_ps2.a" beetle-vb-libretro/beetle-vb-libretro_ps2.a || { exit 1; }
+cp -f "$REPO_FOLDER/mednafen_vb_libretro_ps2.a" beetle-vb-libretro/beetle-vb-libretro_ps2.a || { exit 1; }
