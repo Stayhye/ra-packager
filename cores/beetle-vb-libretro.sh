9ad98b31f4fea6a97f45510a47a1c1057e5ada9f
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-20 22:58:36 -0500

    Change repository URL and simplify make command
    Updated repository URL and modified make command.

--- cores/beetle-vb-libretro.sh
+++ cores/beetle-vb-libretro.sh
@@ -3,7 +3,7 @@
 
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-REPO_URL="https://github.com/libretro/beetle-vb-libretro"
+REPO_URL="https://github.com/Stayhye/beetle-vb-libretro"
 REPO_FOLDER="beetle-vb-libretro"
 BRANCH_NAME="master"
 
@@ -16,9 +16,9 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
-## Compile core
-make -f Makefile.libretro -j $PROC_NR platform=ps2 clean || { exit 1; }
-make -f Makefile.libretro -j $PROC_NR platform=ps2 || { exit 1; }
+## Compile core (removed -f Makefile.libretro since the file is just Makefile)
+make -j $PROC_NR platform=ps2 clean || { exit 1; }
+make -j $PROC_NR platform=ps2 || { exit 1; }
 
 ## Package all nested object files recursively into the archive
 rm -f libretro_ps2.a
