db3e2983df21c3f4caf11ece8eea88052efb5e8b
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-21 08:00:10 -0500

    Update mgba.sh

--- cores/mgba.sh
+++ cores/mgba.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 # package.sh by Francisco Javier Trujillo Mata (fjtrujy@gmail.com)
 
-## Determine the maximum number of processes that Make can work with.
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-## Download the source code.
-REPO_URL="https://github.com/libretro/mgba"
+REPO_URL="https://github.com/mgba-emu/mgba"
 REPO_FOLDER="mgba"
 BRANCH_NAME="master"
+
 if test ! -d "$REPO_FOLDER"; then
-	git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || { exit 1; }
-else
-	cd $REPO_FOLDER && git fetch origin && git reset --hard origin/${BRANCH_NAME} && git checkout ${BRANCH_NAME} || { exit 1; }
+    git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER || { exit 1; }
 fi
 
-## Compile core
-make -f Makefile.libretro -j $PROC_NR platform=ps2 clean || { exit 1; }
-make -f Makefile.libretro -j $PROC_NR platform=ps2 || { exit 1; }
+cd $REPO_FOLDER || { exit 1; }
+git fetch origin
+git reset --hard origin/${BRANCH_NAME}
+git checkout ${BRANCH_NAME} || { exit 1; }
+
+## Compile core using the correct path to Makefile.libretro
+make -C ports/libretro -f Makefile.libretro -j $PROC_NR platform=ps2 clean || { exit 1; }
+make -C ports/libretro -f Makefile.libretro -j $PROC_NR platform=ps2 || { exit 1; }
 
 cd .. || { exit 1; }
+
+## Copy the generated static archive to both locations
+cp -f "$REPO_FOLDER/ports/libretro/mgba_libretro_ps2.a" ./libretro_ps2.a || { exit 1; }
+
+mkdir -p mgba
+cp -f "$REPO_FOLDER/ports/libretro/mgba_libretro_ps2.a" mgba/mgba_libretro_ps2.a || { exit 1; }
