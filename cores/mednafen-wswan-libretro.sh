614b51b65949c57ba0173c033ba83b524a5b17de
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-21 10:49:52 -0500

    Update mednafen-wswan-libretro.sh

--- cores/mednafen-wswan-libretro.sh
+++ cores/mednafen-wswan-libretro.sh
@@ -6,7 +6,7 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 ## Download the source code.
 REPO_URL="https://github.com/Stayhye/beetle-wswan-libretro.git"
-REPO_FOLDER="mednafen-wswan-libretro"
+REPO_FOLDER="beetle-wswan-libretro"
 BRANCH_NAME="master"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || { exit 1; }
