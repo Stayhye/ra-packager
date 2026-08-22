9808622b32bcb45f4107e24a59f68c4328b7d2ee
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-21 21:50:53 -0500

    4

--- package.sh
+++ package.sh
@@ -9,6 +9,10 @@
 ./cores/ecwolf.sh || { exit 1; }
 ./generate_retroarch.sh ecwolf ecwolf_libretro_ps2 || { exit 1; }
 
+#genesis_plus_gx_paprium_libretro
+./cores/genesis_plus_gx_paprium_libretro.sh || { exit 1; }
+./generate_retroarch.sh genesis_plus_gx_paprium_libretro genesis_plus_gx_paprium_libretro_ps2 || { exit 1; }
+
 #mednafen-pce-libretro
 ./cores/mednafen-pce-libretro.sh || { exit 1; }
 ./generate_retroarch.sh mednafen-pce-libretro mednafen-pce-libretro_ps2 || { exit 1; }
