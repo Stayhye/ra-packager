70590ab22f1f45ea851ba796d7adc2b165c6ec42
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-23 14:41:07 -0500

    potator

--- package.sh
+++ package.sh
@@ -29,9 +29,9 @@
 ./cores/genesis_plus_gx_libretro.sh || { exit 1; }
 ./generate_retroarch.sh genesis_plus_gx_libretro genesis_plus_gx_libretro_ps2 || { exit 1; }
 
-#potator_libretro
-./cores/potator_libretro.sh || { exit 1; }
-./generate_retroarch.sh potator_libretro potator_libretro_ps2 || { exit 1; }
+#potator
+./cores/potator.sh || { exit 1; }
+./generate_retroarch.sh potator potator-libretro_ps2 || { exit 1; }
 
 #mednafen-pce-libretro
 ./cores/mednafen-pce-libretro.sh || { exit 1; }
