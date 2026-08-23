07dd8dd47896ec30da33ded9c041b80e3aa98ca7
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-22 22:24:51 -0500

    Update mame2003-plus-libretro.sh

--- cores/mame2003-plus-libretro.sh
+++ cores/mame2003-plus-libretro.sh
@@ -16,9 +16,9 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME} 
 git checkout ${BRANCH_NAME} || { exit 1; } 
 
-## Compile core using native platform=ps2 support with size optimization and dead-code elimination
-make -j $PROC_NR platform=ps2 CFLAGS="-Os -ffunction-sections -fdata-sections" LDFLAGS="-Wl,--gc-sections" clean || { exit 1; } 
-make -j $PROC_NR platform=ps2 CFLAGS="-Os -ffunction-sections -fdata-sections" LDFLAGS="-Wl,--gc-sections" || { exit 1; } 
+## Compile core using native platform=ps2 support 
+make -j $PROC_NR platform=ps2 clean || { exit 1; } 
+make -j $PROC_NR platform=ps2 || { exit 1; } 
 
 cd .. || { exit 1; } 
 
