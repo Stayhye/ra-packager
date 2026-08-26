09a7efdc452853fa75fc5ce9d6f8831ed0657858
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-25 21:30:24 -0500

    Update geargrafx.sh

--- cores/geargrafx.sh
+++ cores/geargrafx.sh
@@ -16,6 +16,9 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
+## Navigate into the platforms/libretro directory where the Makefile is located
+cd platforms/libretro || { exit 1; }
+
 ## Compile core using native platform=ps2 support
 make -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -j $PROC_NR platform=ps2 || { exit 1; }
