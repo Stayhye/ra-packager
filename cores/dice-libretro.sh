751f85693285646d2d7a4560644b2abe29ad560b
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-25 20:41:33 -0500

    Update dice-libretro.sh

--- cores/dice-libretro.sh
+++ cores/dice-libretro.sh
@@ -5,7 +5,7 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 REPO_URL="https://github.com/Stayhye/dice-libretro"
 REPO_FOLDER="dice-libretro"
-BRANCH_NAME="master"
+BRANCH_NAME="main"
 
 if test ! -d "$REPO_FOLDER"; then
     git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER || { exit 1; }
