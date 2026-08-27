3d9e220245dceeb4b4f94475f718b4d699a715a4
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-27 16:05:29 -0500

    Update BennuGD_libretro.sh

--- cores/BennuGD_libretro.sh
+++ cores/BennuGD_libretro.sh
@@ -25,7 +25,7 @@ export CXXFLAGS="-O3 -G0 -ffat-lto-objects"
 cmake .. \
     -DCMAKE_TOOLCHAIN_FILE="${PS2DEV}/share/ps2dev.cmake" \
     -DTHREADS_HAVE_PTHREAD_ARG=OFF \
-    -DCMAKE_BUILD_TYPE=Release || { exit 1; }
+    -DCMAKE_BUILD_TYPE=Release || { exit 1; } 
 
 # Build the project core target
 make -j $PROC_NR || { exit 1; }
