ad6cb5f743ff1f8b90cec4d5db9fe4e8cc5282a4
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-27 08:32:19 -0500

    Update zc210-libretro.sh

--- cores/zc210-libretro.sh
+++ cores/zc210-libretro.sh
@@ -5,7 +5,7 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 REPO_URL="https://github.com/Stayhye/zc210-libretro"
 REPO_FOLDER="zc210-libretro"
-BRANCH_NAME="master"
+BRANCH_NAME="main"
 
 if test ! -d "$REPO_FOLDER"; then
     git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER || { exit 1; }
