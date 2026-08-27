b1533d2360f361f20157e74734becd90352996d6
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-27 08:35:40 -0500

    Update zc210-libretro.sh

--- cores/zc210-libretro.sh
+++ cores/zc210-libretro.sh
@@ -16,9 +16,9 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
-## Compile core using native platform=ps2 support
-make -j $PROC_NR platform=ps2 clean || { exit 1; }
-make -j $PROC_NR platform=ps2 || { exit 1; }
+## Compile core using native platform=ps2 support with implicit declarations allowed as a warning
+make -j $PROC_NR platform=ps2 CFLAGS="-Dps2_clock=clock" NOWERROR=1 clean || { exit 1; }
+make -j $PROC_NR platform=ps2 CFLAGS="-Dps2_clock=clock" NOWERROR=1 || { exit 1; }
 
 cd .. || { exit 1; }
 
