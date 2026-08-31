14b99c1a3d0447bde40f4cecb2c85bcdfec035c5
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-08-31 18:54:17 -0500

    Update package.sh

--- package.sh
+++ package.sh
@@ -10,8 +10,8 @@
 ./generate_retroarch.sh ecwolf ecwolf_libretro_ps2 || { exit 1; }
 
 #race
-./cores/race.sh || { exit 1; }
-./generate_retroarch.sh race_libretro race_libretro_ps2 || { exit 1; }
+./cores/race_libretro.sh || { exit 1; }
+./generate_retroarch.sh race race_libretro_ps2 || { exit 1; }
 
 #geargrafx
 ./cores/geargrafx.sh || { exit 1; }
