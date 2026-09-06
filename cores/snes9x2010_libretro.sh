54fc6b510837bf4b989dbae35f283177588e35ab
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-06 12:16:32 -0500

    Update snes9x2010_libretro.sh

--- cores/snes9x2010_libretro.sh
+++ cores/snes9x2010_libretro.sh
@@ -47,4 +47,6 @@ fi
 cp -f "$FOUND_ARCHIVE" ./libretro_ps2.a || { exit 1; }
 
 mkdir -p snes9x2010_libretro
-cp -f "$FOUND_ARCHIVE" snes9x2010_libretro/snes9x2010_libretro_ps2.a || { exit 1; }
+if [ "$FOUND_ARCHIVE" != "snes9x2010_libretro/snes9x2010_libretro_ps2.a" ]; then
+    cp -f "$FOUND_ARCHIVE" snes9x2010_libretro/snes9x2010_libretro_ps2.a || { exit 1; }
+fi
