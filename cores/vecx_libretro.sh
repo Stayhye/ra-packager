ce156be32dc89b3ca3b01d6059ddf579e55e194d
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-12 10:04:37 -0500

    Update vecx_libretro.sh

--- cores/vecx_libretro.sh
+++ cores/vecx_libretro.sh
@@ -36,9 +36,9 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
-## Compile core using native platform=ps2 support, injecting fake GL headers via CFLAGS/CPPFLAGS
-make -j $PROC_NR platform=ps2 CFLAGS="-I$FAKE_GL_PATH" CPPFLAGS="-I$FAKE_GL_PATH" clean || { exit 1; }
-make -j $PROC_NR platform=ps2 CFLAGS="-I$FAKE_GL_PATH" CPPFLAGS="-I$FAKE_GL_PATH" || { exit 1; }
+## Compile core using native platform=ps2 support, injecting fake GL headers and einline macro definition via CFLAGS/CPPFLAGS
+make -j $PROC_NR platform=ps2 CFLAGS="-I$FAKE_GL_PATH -Deinline=inline" CPPFLAGS="-I$FAKE_GL_PATH -Deinline=inline" clean || { exit 1; }
+make -j $PROC_NR platform=ps2 CFLAGS="-I$FAKE_GL_PATH -Deinline=inline" CPPFLAGS="-I$FAKE_GL_PATH -Deinline=inline" || { exit 1; }
 
 cd .. || { exit 1; }
 
