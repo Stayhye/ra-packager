f6ab321630463067192ae823616491202e0ebd7d
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-17 10:48:49 -0500

    Update mednafen_pcfx_libretro.sh

--- cores/mednafen_pcfx_libretro.sh
+++ cores/mednafen_pcfx_libretro.sh
@@ -16,6 +16,8 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
+# Fix implicit declaration of ps2_clock in rthreads.c
+sed -i '/int scond_wait_timeout/i extern int ps2_clock(void);' libretro-common/rthreads/rthreads.c
 
 # Clean previous build artifacts completely
 make clean platform=ps2 || true
@@ -25,7 +27,6 @@ make -j $PROC_NR platform=ps2 LTO=0 USE_LTO=0 HAVE_LTO=0 || { exit 1; }
 
 ## Inspect binary size and sections locally in the script
 FOUND_ARCHIVE=$(find . -name "*.a" | head -n 1)
-
 
 cp -f "$FOUND_ARCHIVE" ./libretro_ps2.a || { exit 1; }
 
