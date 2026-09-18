c51c17f08de43add8a0bf6de47b2c434ac64748e
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-18 16:40:16 -0500

    Update genesis_plus_gx_libretro.sh

--- cores/genesis_plus_gx_libretro.sh
+++ cores/genesis_plus_gx_libretro.sh
@@ -4,7 +4,7 @@
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 REPO_URL="https://github.com/Stayhye/Genesis-Plus-GX"
-REPO_FOLDER="genesis_plus_gx_libretro"
+REPO_FOLDER="genesis_plus_gx"
 BRANCH_NAME="master"
 
 if test ! -d "$REPO_FOLDER"; then
