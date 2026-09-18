aafcd029e0dc219a23775afd7d875c04c0f7d87b
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-18 11:12:04 -0500

    Update mgba.sh

--- cores/mgba.sh
+++ cores/mgba.sh
@@ -5,7 +5,7 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
 REPO_URL="https://github.com/Stayhye/mgba"
 REPO_FOLDER="mgba"
-BRANCH_NAME="master"
+BRANCH_NAME="ps2"
 
 if test ! -d "$REPO_FOLDER"; then
     git clone --recurse-submodules --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER || { exit 1; }
