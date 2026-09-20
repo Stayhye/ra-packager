d11ebb1da8c99603fe8246ac716e6197e2ccf2ff
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-19 20:13:40 -0500

    Update gearboy.sh

--- cores/gearboy.sh
+++ cores/gearboy.sh
@@ -17,7 +17,7 @@ git fetch origin
 git reset --hard origin/${BRANCH_NAME}
 git checkout ${BRANCH_NAME} || { exit 1; }
 
-cd src/libretro || { exit 1; }
+cd platforms/libretro || { exit 1; }
 ## Compile core
 make -f Makefile -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -f Makefile -j $PROC_NR platform=ps2 || { exit 1; }
