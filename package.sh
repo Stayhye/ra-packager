a0909a1ec0c8bb0adc9ae43752def096d5baab3a
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-19 21:28:18 -0500

    mrboom

--- package.sh
+++ package.sh
@@ -14,7 +14,7 @@
 ./generate_retroarch.sh 81_libretro 81_libretro_ps2 || { exit 1; } 
 
 #mrboom_libretro
-./cores/mrboom-libretro.sh || { exit 1; }
+./cores/mrboom_libretro.sh || { exit 1; }
 ./generate_retroarch.sh mrboom_libretro mrboom_libretro_ps2 || { exit 1; }
 
 #freeintv
