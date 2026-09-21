e110db19a771fe1b0eb35752b91da0febe4954ce
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-21 16:20:56 -0500

    Update gearlynx.sh

--- cores/gearlynx.sh
+++ cores/gearlynx.sh
@@ -3,7 +3,7 @@
 
 PROC_NR=$(getconf _NPROCESSORS_ONLN)
 
-REPO_URL="https://github.com/Stayhye/gearlynx"
+REPO_URL="https://github.com/Stayhye/Gearlynx"
 REPO_FOLDER="gearlynx"
 BRANCH_NAME="master"
 
