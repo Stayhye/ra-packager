b6916bea8d17753bf5dc4c805f3c0a21f6e7e8ae
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-21 16:32:14 -0500

    Update gearlynx.sh

--- cores/gearlynx.sh
+++ cores/gearlynx.sh
@@ -23,5 +23,6 @@ make -f Makefile -j $PROC_NR platform=ps2 clean || { exit 1; }
 make -f Makefile -j $PROC_NR platform=ps2 || { exit 1; }
 
 # Move the generated file up two levels so it sits alongside the gearboy folder
-mv gearboy_libretro_ps2.a ../../ || { exit 1; }
+mv gearlynx_libretro_ps2.a ../../ || { exit 1; }
 
+cd ../..
