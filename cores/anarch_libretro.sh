ba610fabd2a5a0c7ac46f900b2d43c66ae6943af
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-09-21 17:23:32 -0500

    Update anarch_libretro.sh

--- cores/anarch_libretro.sh
+++ cores/anarch_libretro.sh
@@ -23,10 +23,13 @@ mkdir -p build && cd build || { exit 1; }
 cmake .. -DCMAKE_SYSTEM_NAME=PS2 -DCMAKE_BUILD_TYPE=Release || { exit 1; }
 cmake --build . -- -j $PROC_NR || { exit 1; }
 
-## Copy and rename the compiled library to the repository root directory
+## Go back to the repository root folder
+cd ..
+
+## Copy and rename the compiled library to the workspace root directory
 if [ -f "build/anarch_libretro_ps2.a" ]; then
     cp build/anarch_libretro_ps2.a ../anarch_libretro_ps2.a
 else
-    # Fallback to catch any variant naming convention in the build folder
+    # Fallback to catch any variant naming convention
     find build -name "*.a" -exec cp {} ../anarch_libretro_ps2.a \;
 fi
